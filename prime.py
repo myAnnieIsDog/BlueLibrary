5a3ec0223c32a1ea962b38e3af795c327d5f4166
myAnnieIsDog <146695251+myAnnieIsDog@users.noreply.github.com>
2024-01-06 09:27:03 -0800

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,31 @@
+import math
+
+def run():
+    while True:
+        try: 
+            n = int(input("What number would you like to test?\n"))
+            if type(n) != int or n < 2:
+                raise Exception
+            result = (f"{n} is {"prime." if is_prime(n) else "not prime"}")
+            break
+        except:
+            print("\nYou must enter a positive integer")
+            continue
+    print(result)
+
+
+def is_prime(number: int) -> bool:
+    if number < 2:
+        return False
+    if number < 4:        # 2 and 3 are primes
+        return True
+    elif not number % 2:  # All even numbers are not primes
+        return False
+
+    odd_numbers = range(3, int(math.sqrt(number) + 1), 2)
+
+    return not any(not number % i for i in odd_numbers)
+    # from https://github.com/TheAlgorithms/Python/ (I forgot which file)
+
+if __name__ == '__main__':
+    run()
