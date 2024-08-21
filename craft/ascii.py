bd39239036f1038c9b7647b41607ca8d2e4afc86
myAnnieIsDog <146695251+myAnnieIsDog@users.noreply.github.com>
2024-08-21 07:55:08 -0700

    Update ascii.py

--- craft/ascii.py
+++ craft/ascii.py
@@ -1,50 +1,28 @@
+import unittest
+
+
 def run():
-    """print()
-    for i in ascii_meta():
-        print(i)"""
-
-    alpha()
-
-    # for i in ascii_list():
-    #     print(i, end="")
-
-
-all = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-
-
-def alpha():
-    for i in all:
-        if i.isalpha():
-            print(i, end="")
+    print(ascii_list())
+    unittest.main()
 
 
 def ascii_list(start=32, end=127):
-    ascii = []
+    ascii = ""
 
     if not ((start < end) and (32 <= start) and (end <= 127)):
         raise ValueError
 
     for i in range(start, end):
-        ascii.append(chr(i))
+        ascii += chr(i)
     return ascii
 
 
-def ascii_meta(start=32, end=127):
-    map = {}
-    meta_list = []
-
-    if not ((start < end) and (32 <= start) and (end <= 127)):
-        raise ValueError
-
-    for i in range(start, end):
-        map[str(i).zfill(3)] = chr(i)
-
-    for key, value in map.items():
-        b = str(bin(int(key))).replace("0b", "").zfill(8)
-        text = b + key + " = " + value
-        meta_list.append(text)
-
-    return meta_list
+class TestASCII(unittest.TestCase):
+    def test_ascii(self):
+        self.assertEqual(
+            ascii_list(),
+            " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+        )
 
 
 if __name__ == "__main__":
